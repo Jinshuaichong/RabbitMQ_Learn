e4c106229e4c5adb0ec0820de633865bee07c0ed
jinshuaichong <16086243@qq.com>
2022-10-01 10:07:16 +0800

    direct叫环节

---
+++ src/main/java/com/jessca/rabbitmq/six/DirectLogs.java
@@ -0,0 +1,29 @@
+package com.jessca.rabbitmq.six;
+
+import com.jessca.rabbitmq.util.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author jessca
+ * 生成消息发给交换机 扇出交换机和RoutingKey无关
+ */
+public class DirectLogs {
+    public static final String EXCHANGE_NAME="direct_logs";
+    
+    public static void main(String[] args) throws IOException, TimeoutException {
+    
+        Channel channel = RabbitMqUtils.getChannel();
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            String message=scanner.next();
+            channel.basicPublish(EXCHANGE_NAME,"warning",null,message.getBytes());
+            System.out.println("生产者发出消息："+message);
+        }
+    
+    
+    }
+}
---
+++ src/main/java/com/jessca/rabbitmq/six/ReceiveLogsDirect2.java
@@ -0,0 +1,33 @@
+package com.jessca.rabbitmq.six;
+
+import com.jessca.rabbitmq.util.RabbitMqUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author jessca
+ */
+public class ReceiveLogsDirect2 {
+    
+    public static final String EXCHANGE_NAME="direct_logs";
+    
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Channel channel = RabbitMqUtils.getChannel();
+        //声明一个交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        //声明一个队列 临时队列 队列的名称是随机的 当消费者断开与队列的链接的时候队列就自动删除
+        channel.queueDeclare("disk",false,false,false,null);
+        channel.queueBind("disk",EXCHANGE_NAME,"error");
+    
+        DeliverCallback deliverCallback=(consumerTag, message)->{
+            System.out.println("ReceiveLogsDirect2控制台打印接收到的消息："+new String(message.getBody(),"UTF-8"));
+        };
+        
+        channel.basicConsume("disk",deliverCallback,consumerTag->{});
+        
+    }
+}
